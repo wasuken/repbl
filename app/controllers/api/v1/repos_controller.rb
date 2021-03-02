fabe5e703a1597702d382bbb24bc459d6b8bc997
wasuken <wasuken1@gmail.com>
2021-03-02 18:42:25 +0900

    dev implement backend api.

--- app/controllers/api/v1/repos_controller.rb
+++ app/controllers/api/v1/repos_controller.rb
@@ -6,6 +6,33 @@ class Api::V1::ReposController < ApplicationController
   def index
     render json: Repo.select('id, url, title')
   end
+  def recommended
+    repo_id = params[:repo_id]
+    rfile_id = params[:rfile_id]
+    unless (rfile_id && Rfile.exists?(id: rfile_id))
+      render json: {message: "Not found id in Parameters."}, status: 400
+      return
+    end
+    rfile = Rfile
+              .joins(:path)
+              .select('paths.name as name, rfiles.id as rid')
+              .where('rid = ?', rfile_id)
+              .first
+    q = rfile.name.split('/')[-1].split('.')[0].gsub(/-[0-9]+$/, '')
+    recs = Rfile.joins(:path)
+             .joins("inner join repo_paths on repo_paths.path_id = paths.id")
+             .where(repo_paths: {repo_id: repo_id})
+             .where('paths.name like ?', "%#{q}%")
+             .select("rfiles.contents as contents, rfiles.id as id, paths.name as name")
+             .take(6)
+    render json: recs.map{ |rec|
+      {
+        id: rec.id,
+        contents: rec.contents[0..29],
+        name: File.basename(rec.name)
+      }
+    }
+  end
   def show
     zfs = repos_to_zfs(params[:id])
     render json: zfs.to_h
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'repos', to: 'repos#index'
       get 'repos/:id', to: 'repos#show'
+      get 'repos/recommended/:repo_id/:rfile_id', to: 'repos#recommended'
       post 'repos', to: 'repos#create'
       delete 'repos/:id', to: 'repos#destroy'
 
